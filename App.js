408b5642a5a53d11e56b67e9698e70f60693da86
Davor Cortés <davorcortes.dc@gmail.com>
2020-07-03 20:12:22 -0500

    mostrando un listado de objetos

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
 const App = () => {
+  // definir el state de citas
+  const [citas, setCitas] = useState([
+    {id: '1', paciente: 'Hook', propietario: 'Juan', sintomas: 'No Come'},
+    {id: '2', paciente: 'Redux', propietario: 'Itzel', sintomas: 'No Duerme'},
+    {id: '3', paciente: 'Native', propietario: 'Josue', sintomas: 'No Canta'},
+  ]);
+
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de Citas</Text>
+
+      {citas.map(cita => (
+        <View>
+          <Text>{cita.paciente}</Text>
+        </View>
+      ))}
     </View>
   );
 };
